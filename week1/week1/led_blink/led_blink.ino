12dc345e49965e881c2c4b31e51f556b7ab6551e
samridhsapolia <samridhsapolia@gmail.com>
2026-07-02 00:06:16 +0530

    feat: add serial blink counter (v1.1)

--- week1/week1/led_blink/led_blink.ino
+++ week1/week1/led_blink/led_blink.ino
@@ -1,10 +1,20 @@
+int blinkCount = 0;
+
 void setup() {
   pinMode(13, OUTPUT);
+  Serial.begin(9600);
 }
 
 void loop() {
+
   digitalWrite(13, HIGH);
   delay(500);
+
   digitalWrite(13, LOW);
   delay(500);
+
+  blinkCount++;
+
+  Serial.print("Blink count: ");
+  Serial.println(blinkCount);
 }
